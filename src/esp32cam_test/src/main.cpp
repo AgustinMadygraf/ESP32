f6041db01a980c3417ffe6395674da1f3d71fd90
AgustinMadygraf <agustin.mtto.madygraf>
2024-06-08 22:52:30 -0300

    chore: Update platformio.ini with direct URLs for AsyncTCP and LittleFS libraries

--- src/esp32cam_test/src/main.cpp
+++ src/esp32cam_test/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h> // Include the necessary header file
+#include <FS.h>
+#include <SPIFFS.h>  
 
 #define LED_PIN 4
 
@@ -8,8 +10,8 @@ void setup() {
 
 void loop() {
     digitalWrite(LED_PIN, HIGH); // Enciende el LED
-    delay(1000); // Espera un segundo
+    delay(100); // Espera un segundo
     digitalWrite(LED_PIN, LOW); // Apaga el LED
-    delay(1000); // Espera un segundo
+    delay(100); // Espera un segundo
 }
 
