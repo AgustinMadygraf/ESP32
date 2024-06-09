0a449ed9a9bf0e13a300ad8bd39c6b89d909b169
AgustinMadygraf <agustin.mtto.madygraf>
2024-06-09 02:15:33 -0300

    chore: Update platformio.ini and main.ino for ESP32-CAM project

--- src/esp32cam_test/src/main.ino
+++ src/esp32cam_test/src/main.ino
@@ -1,17 +1,33 @@
 #include <Arduino.h> // Include the necessary header file
 
-#define LED_PIN 4
+#define LED_PIN 4 // Define the LED pin
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT); // Configura el pin como salida
+    // Inicializa el monitor serie a 115200 baudios
     Serial.begin(115200);
+    
+    // Espera 2 segundos para dar tiempo a que la conexión serie se establezca
+    delay(2000);
+
+    // Configura el pin LED como salida
+    pinMode(LED_PIN, OUTPUT); 
+
+    // Mensaje de inicialización
+    Serial.println("Inicializando...");
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH); // Enciende el LED
-    delay(1000); // Espera un segundo
-    digitalWrite(LED_PIN, LOW); // Apaga el LED
-    delay(1000); // Espera un segundo
-    Serial.println("Hola mundo");
-}
+    // Enciende el LED
+    digitalWrite(LED_PIN, HIGH); 
+    // Mensaje indicando que el LED está encendido
+    Serial.println("LED encendido");
+    // Espera un segundo
+    delay(1000); 
 
+    // Apaga el LED
+    digitalWrite(LED_PIN, LOW); 
+    // Mensaje indicando que el LED está apagado
+    Serial.println("LED apagado");
+    // Espera un segundo
+    delay(1000); 
+}
