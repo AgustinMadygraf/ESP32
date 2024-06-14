bf9c38e921bf1e3f8c7dab6db7acced4608c7d62
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-06-14 15:59:48 -0300

    chore: Update .gitignore to include src/espwroom32_v1/ directory

---
+++ src/espwroom32_v1/src/red_wifi.h
@@ -0,0 +1,7 @@
+#include <WiFi.h>
+
+// Configura el SSID y la contraseña de tu red WiFi
+const char* ssid = "Aula tecnica";
+const char* password = "Madygraf32";
+
+WebServer server(80); // Puerto 80 para el servidor web
